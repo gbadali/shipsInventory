a2bfabcb038f7d72ffb628424c1cc83fcd3e6691
gbadali <gbadali@gmail.com>
2020-02-05 08:53:58 -0500

    Ch 2.4 Routing Requests

--- main.go
+++ main.go
@@ -6,12 +6,28 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Ships Inventory"))
 }
+
+func showItem(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific item in the inventory..."))
+}
+
+func addItem(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Add an item to the inventory(test)..."))
+}
+
+//test
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/item", showItem)
+	mux.HandleFunc("/item/add", addItem)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
