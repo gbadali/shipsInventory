e0de0bdf9009c4674718fb1f355941aedcb0e2c0
gbadali <gbadali@gmail.com>
2020-02-06 17:21:27 -0500

    Ch 3.1 Managing Configuration Settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// do some configuration stuff
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	// make a new mux
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/item", showItem)
 	mux.HandleFunc("/item/add", addItem)
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
