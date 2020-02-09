04d11abaca962e7652296c307a44c63f62b05947
gbadali <gbadali@gmail.com>
2020-02-09 07:44:53 -0500

    Ch 3.3 Dependency Injection

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -22,19 +21,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
 }
 
-func showItem(w http.ResponseWriter, r *http.Request) {
+func (app *application) showItem(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -44,7 +43,7 @@ func showItem(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Dispaly a specific item with ID %d...", id)
 }
 
-func addItem(w http.ResponseWriter, r *http.Request) {
+func (app *application) addItem(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	// do some configuration stuff
 	addr := flag.String("addr", ":4000", "HTTP network address")
@@ -17,14 +22,19 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	// make a new mux
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/item", showItem)
-	mux.HandleFunc("/item/add", addItem)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/item", app.showItem)
+	mux.HandleFunc("/item/add", app.addItem)
 
+	// serve the staic files for css and js
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	srv := &http.Server{
@@ -33,6 +43,7 @@ func main() {
 		Handler:  mux,
 	}
 
+	// start the actual server
 	infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
