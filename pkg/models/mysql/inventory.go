c9ce441ad99de283770176233640fa68a11edaa7
gbadali <gbadali@gmail.com>
2020-02-25 11:37:46 -0500

    Ch 4.5 Executing SQL Statements

--- pkg/models/mysql/inventory.go
+++ pkg/models/mysql/inventory.go
@@ -10,14 +10,28 @@ type InventoryModel struct {
 	DB *sql.DB
 }
 
-func (m *InventoryModel) Insert(itemName, partNum, description, numOnHand string) (int, error) {
-	return 0, nil
+func (m *InventoryModel) Insert(itemName, partNum, description, site, space, drawer string, numOnHand int) (int, error) {
+	stmt := `INSERT INTO inventory (itemName, description, created, lastInventory, numOnHand, partNum, site, space, drawer)
+	VALUES(?, ?, UTC_TIMESTAMP(), UTC_TIMESTAMP, ?, ?, ?, ?, ?)`
+
+	result, err := m.DB.Exec(stmt, itemName, description, numOnHand, partNum, site, space, drawer)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, nil
+	}
+
+	return int(id), nil
+
 }
 
 func (m *InventoryModel) Get(id int) (*models.Item, error) {
 	return nil, nil
 }
 
-func (m *InventoryModel) Oldest() ([]*models.Snippet, error) {
+func (m *InventoryModel) Oldest() ([]*models.Item, error) {
 	return nil, nil
 }
