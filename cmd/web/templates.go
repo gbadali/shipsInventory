e50c99181ea562df1c387b6a440399d1b5dec419
gbadali <gbadali@gmail.com>
2020-03-07 05:42:38 -0800

    Ch 5.6 Custom Template Functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/gbadali/shipsInventory/pkg/models"
 )
@@ -26,7 +27,10 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles(page)
+		// here we parse the template files as well as add the functions we define
+		// below so the templates know about the functions.  template.New creates a
+		// new empty template set, then Funcs() registers the FuncMap and then we parse.
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
@@ -44,4 +48,15 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 		cache[name] = ts
 	}
 	return cache, nil
+}
+
+// humanDate returns a nicely formatted string of the time
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// this is a string keyed map which acts as a lookup between the names
+// of our custom template functions and the functions themselves.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
