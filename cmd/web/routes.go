47c08d719f82d5725b02c58886a131e82fab968d
gbadali <gbadali@gmail.com>
2020-03-08 16:57:21 -0700

    Ch 6.2 Setting Security Headers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	// make a new mux
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
@@ -13,5 +13,5 @@ func (app *application) routes() *http.ServeMux {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return mux
+	return secureHeaders(mux)
 }
