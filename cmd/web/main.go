5787527e2a526213265aeb232afe0972e665054d
gbadali <gbadali@gmail.com>
2020-03-15 08:55:10 -0700

    Ch 10.2 running a HTTPs Server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -65,7 +65,7 @@ func main() {
 
 	// start the actual server
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
